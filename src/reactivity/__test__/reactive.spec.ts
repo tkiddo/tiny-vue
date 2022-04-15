22f4f58b7b7f659f91e9cf3239a191c817febb91
Kaiqiang.Tang <Kaiqiang.Tang@geely.com>
2022-04-15 13:27:13 +0800

    feat:isReactive

--- src/reactivity/__test__/reactive.spec.ts
+++ src/reactivity/__test__/reactive.spec.ts
@@ -1,14 +1,44 @@
-import { reactive } from "../reactive";
+import { effect } from "../effect";
+import { isReactive, reactive } from "../reactive";
 
 test("happy path", () => {
   expect(true).toBe(true);
 });
 
 test("reactive function", () => {
-  const observed = reactive({
+  const original = {
     foo: "foo",
-  });
+  };
+  const observed = reactive(original);
+  expect(observed).not.toBe(original);
   expect(observed.foo).toBe("foo");
   observed.foo = "bar";
   expect(observed.foo).toBe("bar");
+});
+
+test("track and trigger", () => {
+  const foo = reactive({
+    number: 0,
+  });
+  let count = 0;
+  const fn = () => {
+    count = foo.number + 1;
+  };
+  const mockCallback = jest.fn(fn);
+  effect(mockCallback);
+  expect(mockCallback).toHaveBeenCalledTimes(1);
+  expect(count).toBe(1);
+  foo.number = 3;
+  expect(mockCallback).toHaveBeenCalledTimes(2);
+  expect(count).toBe(4);
+});
+
+test("is reactive", () => {
+  const original = {
+    name: "hello",
+  };
+  const foo = reactive(original);
+  expect(foo.name).toBe("hello");
+  expect(isReactive(foo)).toBe(true);
+  expect(isReactive(original)).toBe(false);
 });
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -2,24 +2,33 @@ import { activeEffect } from "./effect";
 
 const depsMap = new Map();
 
-export function reactive(target: any) {
+const ISREACTIVE = "__is_reactive";
+
+type CustomObject = Record<string, any>;
+
+export function reactive(target: CustomObject) {
   return createReactiveObject(target);
 }
 
-function createReactiveObject(target: any) {
+function createReactiveObject(target: CustomObject) {
   return new Proxy(target, {
     get(target, key, receiver) {
-      track(target, key);
-      return Reflect.get(target, key, receiver);
+      if (key === ISREACTIVE) {
+        return true;
+      } else {
+        track(target, key as string);
+        return Reflect.get(target, key, receiver);
+      }
     },
     set(target, key, value, receiver) {
       Reflect.set(target, key, value, receiver);
-      trigger(target, key);
+      trigger(target, key as string);
       return true;
     },
   });
 }
-function track(target, key) {
+
+function track(target: CustomObject, key: string) {
   let targetMap = depsMap.get(target);
   if (!targetMap) {
     targetMap = new Map();
@@ -33,10 +42,14 @@ function track(target, key) {
   dep.add(activeEffect);
 }
 
-function trigger(target, key) {
+function trigger(target: CustomObject, key: string) {
   const targetMap = depsMap.get(target);
   const dep = targetMap.get(key);
-  dep.forEach((fn) => {
+  dep.forEach((fn: unknown) => {
     typeof fn === "function" && fn();
   });
+}
+
+export function isReactive(source: any) {
+  return !!source[ISREACTIVE];
 }
