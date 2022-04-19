f24354f77265563517518e81dfe9b9af789f2a35
Kaiqiang.Tang <Kaiqiang.Tang@geely.com>
2022-04-19 15:26:37 +0800

    feat: ref start

--- src/reactivity/__test__/ref.spec.ts
+++ src/reactivity/__test__/ref.spec.ts
@@ -1,6 +1,15 @@
+import { effect } from "../effect";
 import { ref } from "../ref";
 
 test("ref happy path", () => {
   const observed = ref(1);
+  let count = 0;
+  effect(() => {
+    count = observed.value + 1;
+  });
   expect(observed.value).toBe(1);
+  expect(count).toBe(2);
+  observed.value = 2;
+  expect(observed.value).toBe(2);
+  expect(count).toBe(3);
 });
--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,6 +1,20 @@
+import { track, trigger } from "./effect";
+
+class RefImpl {
+  private _value;
+  constructor(initialValue: any) {
+    this._value = initialValue;
+  }
+  get value() {
+    track(this, "value");
+    return this._value;
+  }
+  set value(newValue) {
+    this._value = newValue;
+    trigger(this, "value");
+  }
+}
+
 export function ref(source: any) {
-  const result = {
-    value: source,
-  };
-  return result;
+  return new RefImpl(source);
 }
